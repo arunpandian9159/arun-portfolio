b0af52bc8ff616606ff67e6812497d9a0bbb2499
Arunpandian C <arunpandiancse25@gmail.com>
2025-11-07 21:43:58 +0530

    updates

--- components/sections/education-section.tsx
+++ components/sections/education-section.tsx
@@ -14,7 +14,7 @@ const educationData = [
   { 
     institution: "Amalorpavam Higher Secondary School", 
     degree: "Higher Secondary Course (HSC)", 
-    duration: "2020 - 2021",
+    duration: "2020 - 2021", 
     grade: "Percentage - 87%",
     location: "Puducherry",
   },
